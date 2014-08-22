f2172ca5ea119e50f40f050e235089e8e56f3e4e
Henri Bergius <henri.bergius@iki.fi>
2014-08-22 20:07:12 +0200

    Remove old index file from branch

--- app/noflo.js
+++ app/noflo.js
@@ -1,50 +1,17 @@
+console.time('noflo-ui-init');
+console.time('polymer-ready');
 window.addEventListener('polymer-ready', function() {
+  console.timeEnd('polymer-ready');
   document.body.classList.remove('loading');
   window.nofloStarted = false;
 
+  console.time('noflo-prepare');
   var noflo = require('noflo');
-  var graphs = {};
-
-  var registerAndStart = function (network) {
-    for (var component in graphs) {
-      if (component === 'main') {
-        continue;
-      }
-      network.loader.registerComponent('local', component, graphs[component]);
-    }
-    network.once('start', function () {
-      window.nofloStarted = true;
-    });
-    network.connect(function () {
-      network.start();
-    });
-  };
-
-  var fbps = Array.prototype.slice.call(document.querySelectorAll('script[type="application/fbp"]'));
-  fbps.forEach(function (fbp) {
-    var fbpString = fbp.textContent.trim();
-    var fbpId = fbp.getAttribute('id');
-    noflo.graph.loadFBP(fbpString, function (graph) {
-      graphs[fbpId] = graph;
-      graph.name = fbpId;
-      graph.baseDir = '/noflo-ui';
-      if (fbpId === 'main') {
-        noflo.createNetwork(graph, function (network) {
-          registerAndStart(network);
-        }, true);
-      }
+  noflo.graph.loadJSON(require('noflo-ui/graphs/main.fbp'), function (g) {
+    g.baseDir = '/noflo-ui';
+    noflo.createNetwork(g, function (n) {
+      console.timeEnd('noflo-prepare');
+      console.timeEnd('noflo-ui-init');
     });
   });
-
-  // Show help on demand
-  var help = document.querySelector('noflo-help');
-  document.getElementById("openhelp").addEventListener("click", function (event) {
-    if (help) {
-      help.open();
-      event.preventDefault();
-    }
-    // Otherwise follow link to docs
-  });
-
 });
-
