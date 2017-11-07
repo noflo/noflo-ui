7a733939ab10282a8b4c8564d3a6fa5a38ceb998
Henri Bergius <henri.bergius@iki.fi>
2017-11-07 19:43:09 +0100

    Set up SauceLabs via Karma

---
+++ karma.conf-ci.js
@@ -0,0 +1,36 @@
+const externals = require('./externals.conf.js');
+const base = require('./karma.conf.js');
+
+module.exports = function(config) {
+  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+    process.exit(1)
+  }
+
+  // Apply base config
+  base(config);
+
+  // Then add Sauce Labs setup
+  const customLaunchers = {
+    sl_safari: {
+      base: 'SauceLabs',
+      browserName: 'safari',
+      version: '11',
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '56',
+    },
+  };
+  const reporters = config.reporters;
+  reporters.push('saucelabs');
+
+  // Apply additional config
+  config.set({
+    reporters,
+    customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    captureTimeout: 120000,
+  });
+}
