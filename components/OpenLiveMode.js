de08e325b39db2d5943b89d130194590a1228a2d
Henri Bergius <henri.bergius@iki.fi>
2019-02-26 13:13:32 +0100

    Make live mode populate a project

--- components/OpenLiveMode.js
+++ components/OpenLiveMode.js
@@ -1,6 +1,75 @@
 const noflo = require('noflo');
 const uuid = require('uuid/v4');
+const url = require('url');
+const path = require('path');
 const { loadGraph, ensureIframe, getRemoteNodes } = require('../src/runtime');
+
+const getNamespace = (client) => {
+  if (!client.definition || !client.definition.namespace) {
+    return null;
+  }
+  return client.definition.namespace;
+};
+
+const isComponentInProject = (namespace, componentName) => {
+  if (componentName.indexOf('/') === -1 && componentName !== 'Graph') { return true; }
+  const [library] = componentName.split('/');
+  return library === namespace;
+};
+
+const fetchFromLibrary = (namespace, client) => {
+  if (!namespace) { return Promise.resolve([]); }
+  return client.connect()
+    .then(() => client.protocol.component.list())
+    .then(components => components
+      .map(component => component.name)
+      .filter(name => isComponentInProject(namespace, name)))
+    .then(components => Promise.all(components.map(name => client
+      .protocol.component.getsource({
+        name,
+      }))));
+};
+
+const ensureProject = (client, projects) => {
+  const matching = projects.find((proj) => {
+    if (proj.runtime && proj.runtime === client.definition.id) {
+      return true;
+    }
+    return false;
+  });
+  if (matching) {
+    // We already have a project for this runtime
+    return matching;
+  }
+  const project = {
+    id: uuid.v4(),
+    name: getNamespace(client),
+    namespace: getNamespace(client),
+    runtime: client.definition.id,
+    type: client.definition.type,
+    graphs: [],
+    components: [],
+    specs: [],
+  };
+  return project;
+};
+
+const updateProjectMetadata = (client, p) => {
+  const project = p;
+  if (client.definition.repository) {
+    const parsed = url.parse(client.definition.repository);
+    if ((parsed.hostname === 'github.com') && parsed.pathname) {
+      const pathname = parsed.pathname.slice(1);
+      const org = path.dirname(pathname);
+      const repo = path.basename(pathname, path.extname(pathname));
+      project.repo = `${org}/${repo}`;
+      project.name = project.repo;
+    }
+  }
+  if (client.definition.repositoryVersion) {
+    project.branch = client.definition.repositoryVersion;
+  }
+};
 
 exports.getComponent = () => {
   const c = new noflo.Component();
@@ -8,14 +77,24 @@ exports.getComponent = () => {
     { datatype: 'object' });
   c.inPorts.add('client',
     { datatype: 'object' });
+  c.inPorts.add('projects',
+    { datatype: 'object' });
   c.outPorts.add('out',
+    { datatype: 'object' });
+  c.outPorts.add('project',
+    { datatype: 'object' });
+  c.outPorts.add('graph',
+    { datatype: 'object' });
+  c.outPorts.add('component',
+    { datatype: 'object' });
+  c.outPorts.add('runtime',
     { datatype: 'object' });
   c.outPorts.add('error',
     { datatype: 'object' });
 
   return c.process((input, output) => {
-    if (!input.hasData('in', 'client')) { return; }
-    const [route, client] = input.getData('in', 'client');
+    if (!input.hasData('in', 'client', 'projects')) { return; }
+    const [route, client, projects] = input.getData('in', 'client', 'projects');
 
     const state = {
       state: 'ok',
@@ -37,7 +116,8 @@ exports.getComponent = () => {
           return Promise.reject(new Error(`Runtime ${def.id} is not running a graph`));
         }
 
-        // TODO: Populate project information
+        state.project = ensureProject(client, projects);
+        updateProjectMetadata(client, state.project);
 
         return client.protocol.component.getsource({
           name: def.graph,
@@ -47,9 +127,73 @@ exports.getComponent = () => {
         ...source,
         name: client.definition.graph, // Ensure graph gets the name runtime supplied
       }))
-      .then(graphInstance => state.graphs.push(graphInstance))
+      .then((graphInstance) => {
+        graphInstance.setProperties({
+          id: `${state.project.id}/${(graphInstance.properties != null ? graphInstance.properties.id : undefined) || graphInstance.name}`,
+          project: state.project.id,
+        });
+        state.graphs.push(graphInstance);
+        state.project.graphs.push(graphInstance);
+        state.project.main = graphInstance.properties.id;
+      })
+      .then(() => fetchFromLibrary(state.project.namespace, client))
+      .then((sources) => {
+        const projectGraphs = sources.filter((component) => {
+          if (component.language !== 'json') {
+            return false;
+          }
+          if (component.name === client.definition.graph || `${component.library}/${component.name}` === client.definition.graph) {
+            return false;
+          }
+          return true;
+        });
+        return Promise.all(projectGraphs.map(graphDef => loadGraph(graphDef)
+          .then((g) => {
+            const graph = g;
+            graph.name = path.basename(graphDef.name);
+            graph.setProperties({
+              id: `${state.project.id}/${(graph.properties != null ? graph.properties.id : undefined) || graph.name}`,
+              project: state.project.id,
+            });
+            return graph;
+          })))
+          .then((graphs) => {
+            graphs.forEach((g) => {
+              state.project.graphs.push(g);
+            });
+            const components = sources.filter(comp => comp.language !== 'json');
+            components.forEach((comp) => {
+              const component = comp;
+              component.project = state.project.id;
+              state.project.components.push(component);
+            });
+          });
+      })
+      .then(() => {
+        // Associate runtime with project for auto-connecting
+        client.definition.project = state.project.id;
+        client.definition.seen = new Date();
+        if (!client.definition.label) {
+          client.definition.label = `${state.project.name} runtime`;
+        }
+      })
       .then(() => getRemoteNodes(client, state))
-      .then(() => output.send({ out: state }))
+      .then(() => output.send({
+        out: state,
+        project: state.project,
+      }))
+      .then(() => {
+        state.project.graphs.forEach((graph) => {
+          output.send({
+            graph,
+          });
+        });
+        state.project.components.forEach((component) => {
+          output.send({
+            component,
+          });
+        });
+      })
       .then((() => output.done()), err => output.done(err));
   });
 };
