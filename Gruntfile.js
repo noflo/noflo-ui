05ff372789b39359a8939f4f3289d71845fb431f
WilliamRClark <cwilliamwork@yahoo.com>
2020-10-30 18:30:43 -0400

    Substitute strings in elements

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,11 @@ module.exports = function () {
           './config.xml': './config.dist.xml',
           './manifest.json': './manifest.dist.json',
           './manifest.webapp.json': './manifest.dist.webapp.json',
+          './elements/noflo-account-settings.html': './elements/noflo-account-settings.dist.html',
+          './elements/noflo-account.html': './elements/noflo-account.dist.html',
+          './elements/noflo-context.html': './elements/noflo-context.dist.html',
+          './elements/noflo-new-runtime.html': './elements/noflo-new-runtime.dist.html',
+          './elements/noflo-repo-card.html': './elements/noflo-repo-card.dist.html',
         },
         options: {
           replacements: [{
