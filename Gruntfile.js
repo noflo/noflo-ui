4a59c910c88c4ec67eb02c7d43b2cbfe2379d3c7
Henri Bergius <henri.bergius@iki.fi>
2020-11-04 16:34:31 +0100

    Generate new-style Polymer elements

--- Gruntfile.js
+++ Gruntfile.js
@@ -249,11 +249,11 @@ ga('send', 'pageview');
     },
 
     sharedstylecomponent: {
-      'elements/the-graph-styles.html': [
+      'elements/the-graph-styles.js': [
         'node_modules/the-graph/themes/the-graph-dark.css',
         'node_modules/the-graph/themes/the-graph-light.css',
       ],
-      'elements/codemirror-styles.html': [
+      'elements/codemirror-styles.js': [
         'node_modules/codemirror/addon/lint/lint.css',
         'node_modules/codemirror/lib/codemirror.css',
         'node_modules/codemirror/theme/mdn-like.css',
@@ -277,14 +277,19 @@ ga('send', 'pageview');
   this.registerMultiTask('sharedstylecomponent', 'Combine CSS files into a Polymer shared style element', function () {
     const sources = this.data.map((file) => grunt.file.read(file));
     const template = `\
-<!-- Generated from <%= files %> -->
+/**
+ * Generated from <%= files %>
+ */
+const $_documentContainer = document.createElement('template');
+$_documentContainer.innerHTML = \`
 <dom-module id="<%= id %>">
   <template>
     <style>
       <%= sources %>
     </style>
   </template>
-</dom-module>\
+</dom-module>\`;
+document.head.appendChild($_documentContainer.content);
 `;
     const id = path.basename(this.target, path.extname(this.target));
     const result = grunt.template.process(template, {
@@ -308,8 +313,8 @@ ga('send', 'pageview');
     'sharedstylecomponent',
   ]);
   this.registerTask('build', [
-    'build_noflo',
     'build_polymer',
+    'build_noflo',
     'string-replace:app',
     'compress',
   ]);
